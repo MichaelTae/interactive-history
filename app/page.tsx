f60af0787f88b5ad4b7e6c886c620ebc5cb10a46
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-16 18:33:37 +0200

    css change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Navigation from './components/navigation/navigation';
 export default function Home() {
   const router = useRouter()
   return (
-    <main className="flex min-h-screen flex-col relative border justify-between p-24 bg-[#e6dbce]/20 ">
+    <main className="flex min-h-screen flex-col relative  justify-between p-24 bg-[#e6dbce]/20 ">
      
     <Navigation/>
     
