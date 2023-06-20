6b8b72580ec8be4c37224d8e84672048307263a6
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-20 19:50:43 +0200

    added homebutton to layout

--- app/barracks/layout.tsx
+++ app/barracks/layout.tsx
@@ -1,12 +1,15 @@
+import HomeButton from "../components/button/homeButton";
+
 export default function BarracksLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className='flex flex-col md:flex-row justify-center bg-slate-800 border  min-h-screen   min-w-full '>
+    <section className='flex flex-col md:flex-row justify-center bg-slate-800   min-h-screen   min-w-full '>
     
         <div className="absolute top-20 self-center">
+          <HomeButton />
         {children}
         </div>
     </section>
