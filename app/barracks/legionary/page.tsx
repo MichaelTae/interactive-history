f521da93c49e62599390c3c99f9e98c9db790eba
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-20 20:18:05 +0200

    finished legionary page

--- app/barracks/legionary/page.tsx
+++ app/barracks/legionary/page.tsx
@@ -1,13 +1,66 @@
+'use client';
+
+import CustomButton from "@/app/components/button/customButton";
+import LegionarySvg from "../../components/LegionarySVG";
+import { useState } from "react";
+import Modal from '../../components/modal/modal'
+
+
 
 
 
 const LegionaryPage = () => {
+  const [highlighted, setHighlighted] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
 
-    return (
-        <div>
-            <h1>Legionary</h1>
-        </div>
-    )
+  const handleOpenModal = (title:string) => {
+    setModalTitle(title);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  
+  return (
+    <div className='flex flex-col relative w-screen  '>
+      <h1 className='flex self-center mb-1 text-3xl'>Legionary</h1>
+      {/* Div below will be extra buttons to switch between different soldiers/items */}
+      <div className='flex gap-2 self-center border w-3/4 justify-center '><div>PlaceHolder</div></div> 
+
+      <div className="flex  w-3/4 self-center border h-[70vh] ">
+      
+     
+      <div className='flex justify-center flex-col items-center gap-12 basis-1/4 '>
+      <CustomButton title='Lorica Segmentata'sethighlighted={setHighlighted} openModal={handleOpenModal} />
+      <CustomButton title='Caligae'sethighlighted={setHighlighted} openModal={handleOpenModal} />
+      <CustomButton title='Galea'sethighlighted={setHighlighted} openModal={handleOpenModal} />
+      
+      </div>
+        {/* This div is for the image/svg of the soldier/item */}
+      <div className='flex border justify-center items-center flex-col  gap-12 basis-2/4'>
+        <div className="flex relative w-full h-full pt-12 justify-center overflow-hidden">
+          
+          
+          <LegionarySvg highlighted={highlighted}/> 
+        
+       
+       
+       </div>
+      </div>
+
+    <Modal title={modalTitle} isOpen={isModalOpen} onClose={handleCloseModal} type={'Legionary'} />  
+      <div className='flex justify-center items-center flex-col gap-12  basis-1/4'>
+      <CustomButton title='Cingulum Militare'sethighlighted={setHighlighted} openModal={handleOpenModal} /> 
+      <CustomButton title='Scutum'sethighlighted={setHighlighted}  openModal={handleOpenModal}/> 
+      <CustomButton title='Weaponry'sethighlighted={setHighlighted} openModal={handleOpenModal} />
+      </div>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default LegionaryPage;
