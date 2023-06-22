267a42a126529b9b58d24c7182d2e8289201a09b
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-22 19:20:47 +0200

    modal css changes

--- app/components/modal/modal.tsx
+++ app/components/modal/modal.tsx
@@ -1,6 +1,6 @@
 
 import ModalContent from "./modalContent";
-
+import {  motion } from "framer-motion";
 
 const Modal = ({ title, isOpen, onClose,type }:{title:string,isOpen:boolean,onClose:any,type:string}) => {
   if (!isOpen) {
@@ -12,17 +12,36 @@ const Modal = ({ title, isOpen, onClose,type }:{title:string,isOpen:boolean,onCl
       onClose();
     }
   };
+  const container ={
+    hidden:{opacity:0,
+    },
+    show:{
+      opacity:1,
+
+      transition:{
+        ease:'easeInOut',
+        duration:0.5
+      }
+    },
+    exit:{
+      opacity:0,
+      
+
+    }
+  }
 
   return (
-    <div onClick={handleOutsideClick} className="fixed z-50 overflow-auto w-screen left-0 top-0 right-0 bottom-0 bg-black backdrop-blur-sm bg-opacity-50 flex items-center justify-center">
-      <div className="bg-slate-700 rounded-lg p-8 m-2 w-full min-h-[500px]   max-w-4xl">
-        <h1 className="text-2xl text-slate-300 font-bold mb-10 text-center">{title}</h1>
+    
+    <motion.div variants={container} initial='hidden' animate='show' exit='exit' onClick={handleOutsideClick} className="fixed z-50  overflow-auto w-screen left-0 top-0 right-0 bottom-0 bg-black backdrop-blur-sm bg-opacity-50 flex items-center justify-center">
+      <div className="bg-slate-700 rounded-lg p-8 m-2 w-full  min-h-[500px] shadow-[10px_10px_16px_0px_rgba(0,0,0,0.9)]   max-w-4xl">
+        <h1 className="text-4xl text-slate-300 font-bold pt-2 rounded-xl pb-12 text-center shadow-[10px_-5px_16px_-10px_rgba(0,0,0,0.9)] font-cinzel ">{title}</h1>
         <ModalContent title={title} type={type} />
-        <button onClick={onClose} className="mt-4 bg-slate-900 border border-slate-600 shadow-inner shadow-slate-700 hover:scale-105 transition-all duration-200  px-4 py-1 rounded">
+        <button onClick={onClose} className="mt-4 bg-slate-900 border hover:text-lime-500 hover:border-lime-500 border-slate-600 shadow-inner shadow-slate-700 hover:scale-105 transition-all duration-300  px-4 py-1 rounded">
           Close 
         </button>
       </div>
-    </div>
+    </motion.div>
+   
   );
 };
 
--- app/components/modal/modalContent.tsx
+++ app/components/modal/modalContent.tsx
@@ -22,15 +22,15 @@ const getCategoryContent = (category: string | number, title: string | number) =
   };
  
   return (
-    <div className='flex text-black relative  w-full h-3/4'>
-      <div className='w-1/2  absolute flex -top-7 text-slate-400 text-xl justify-center '>
+    <div className='flex shadow-[10px_10px_16px_0px_rgba(0,0,0,0.9)]  relative rounded-xl w-full h-3/4'>
+      <div className='w-1/2  absolute flex -top-[30.5px]  text-slate-400 text-2xl justify-center '>
       <div
           onClick={() => setActiveSection('Summary')}
           className={`${
             activeSection === 'Summary'
-              ? ' border-r scale-95 text-slate-300'
+              ? ' border-r scale-95 text-slate-300 !bg-slate-700/100 rounded-t-md   shadow-[inset_3px_2px_2px_0px_rgba(0,0,0,0.4)]'
               : ''
-          } cursor-pointer border-t border-l px-1 rounded-t-lg bg-slate-700/50   border-lime-500`}
+          } cursor-pointer border-t border-l px-1 rounded-bl-sm bg-slate-700/80    border-lime-500/60 hover:text-slate-300 transition-all duration-300`}
         >
           Summary
         </div>
@@ -38,9 +38,9 @@ const getCategoryContent = (category: string | number, title: string | number) =
           onClick={() => setActiveSection('History')}
           className={`${
             activeSection === 'History'
-              ? ' border-r border-l scale-95 text-slate-300'
+              ? ' border-r border-l scale-95 !bg-slate-700/100   text-slate-300 rounded-t-md shadow-[inset_-3px_2px_2px_0px_rgba(0,0,0,0.4)]'
               : ''
-          } cursor-pointer border-t  px-1 rounded-t-lg bg-slate-700/50   border-lime-500`}
+          } cursor-pointer border-t  px-1  bg-slate-700/80   border-lime-500/60 hover:text-slate-300 transition-all duration-300`}
         >
           History
         </div>
@@ -48,25 +48,25 @@ const getCategoryContent = (category: string | number, title: string | number) =
           onClick={() => setActiveSection('Name')}
           className={`${
             activeSection === 'Name'
-              ? 'border-l border-r scale-95 text-slate-300'
+              ? 'border-l border-r scale-95 !bg-slate-700/100 text-slate-300 rounded-t-md shadow-[inset_-3px_2px_2px_0px_rgba(0,0,0,0.4)]'
               : ''
-          } cursor-pointer border-t  px-1 rounded-t-lg bg-slate-700/50   border-lime-500`}
+          } cursor-pointer border-t  px-1  bg-slate-700/80   border-lime-500/60 hover:text-slate-300 transition-all duration-300`}
         >
           Name
         </div>
         <div
           onClick={() => setActiveSection('Usage')}
           className={`${
-            activeSection === 'Usage' ? 'border-l scale-95 text-slate-300' : ''
-          } cursor-pointer border-t border-r px-1 rounded-t-lg bg-slate-700/50   border-lime-500`}
+            activeSection === 'Usage' ? 'border-l scale-95 !bg-slate-700/100  text-slate-300 rounded-t-md shadow-[inset_-3px_2px_2px_0px_rgba(0,0,0,0.4)]' : ''
+          } cursor-pointer border-t border-r px-1  bg-slate-700/80 rounded-br-sm  border-lime-500/60 hover:text-slate-300 transition-all duration-300`}
         >
           Usage
         </div>
       </div>
-      <div className='w-1/2 min-h-[500px] max-h-[500px] text-lg border-lime-500 border-b border-r border-l border-t px-2 bg-slate-700 text-slate-300 overflow-y-auto'>
+      <div className='w-1/2 min-h-[500px] max-h-[500px] text-lg rounded-xl  border-lime-500/60 border-b  border-l border-t px-2 bg-slate-700 text-slate-300 overflow-y-auto'>
         <p>{activeSection === "History" ? history : activeSection === "Usage" ? usage : activeSection === "Name" ? name : description}</p>
       </div>
-      <div className='w-1/2 min-h-[500px] max-h-[500px] relative'>{image && <Image src={image} className=' object-contain  w-full h-full border' placeholder='blur' width={400} height={400} alt={title} />} </div>
+      <div className='w-1/2 min-h-[500px] max-h-[500px]  relative'>{image && <Image src={image} className=' object-contain  w-full h-full border border-lime-500/60 rounded-xl' placeholder='blur' width={400} height={400} alt={title} />} </div>
     </div>
   );
 };
